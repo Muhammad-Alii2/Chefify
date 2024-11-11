2dfefeb7514b6f54cd81d50a2df3813163456734
Jahanzaib-Hyder902 <jhanzaibhyder1@gmail.com>
2024-11-11 21:52:21 +0500

    search bar added.
    search bar added.

---
+++ src/app/components/GalleryCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GalleryCard = () => {
+  return (
+    <>galleryCard</>
+  )
+}
+
+export default GalleryCard
--- src/app/pages/Gallery/page.js
+++ src/app/pages/Gallery/page.js
@@ -1,14 +1,19 @@
 import react from "react";
+import GalleryCard from "@/app/components/GalleryCard";
+import SearchBar from "@/app/components/SearchBar";
 
 const app = () => {
 
     return (
-   <>
-   
-   Hello Ahsan harami 
-   
-   </>
+        <>
+            <div>
+                <SearchBar />
+            </div>
 
+            <div>
+                <GalleryCard />
+            </div>
+        </>
     )
 }
 export default app;
